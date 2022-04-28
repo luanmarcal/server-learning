f0696145bf7bef8575ceeb63746c98363f74ac19
luanmarcal <luan.marcal@padolabs.com.br>
2022-04-28 17:40:48 -0300

    funcionando

--- src/app.js
+++ src/app.js
@@ -3,55 +3,67 @@ const path = require('path');
 const cors = require('cors')
 const { json } = require('body-parser')
 
+const Temperature = require('./models/Temperature')
+
+const temperatures = require('./routes/temperature')
+
 const server = express()
 const mongoose = require(`mongoose`)
-const port = 4002
+const port = 4001
 
 server.use(json())
 server.use(cors())
+server.use('/temperature', temperatures)
+server.use(express.static('public'))
 
-let dummyCount = 0
-let temperatures = [];
+// let dummyCount = 0
+// let temperatures = [];
+
+// server.get('/', (req, res) => {
+//     res.status(200).send(temperatures)
+// })
 
 server.get('/', (req, res) => {
-    res.status(200).send(temperatures)
+    res.status(200).sendFile(path.join(__dirname, "views", "index.html"))
 })
 
-server.post('/', (req, res) => {
-    const request = req.body
+server.use((req, res) => res.status(404).sendFile(path.join(__dirname, "views", "404.html")));
 
-    const temperatureObject = {
-        id: dummyCount += 1,
-        temperature: request.temperature
-    }
-    temperatures.push(temperatureObject)
-    res.status(201).send();
-})
+// server.post('/', (req, res) => {
+//     const request = req.body
 
-server.delete('/', (req, res) => {
-    temperatures = [];
-    res.status(200).send()
-})
+//     const temperatureObject = {
+//         id: dummyCount += 1,
+//         temperature: request.temperature
+//     }
+//     temperatures.push(temperatureObject)
+//     res.status(201).send();
+// })
 
-server.put('/:value', (req, res) => {
-    const value = req.params.value
-    const id = req.query.id
-    console.log(`QUERY IS ${id} AND PARAMETER IS ${value}`)
+// server.delete('/', (req, res) => {
+//     temperatures = [];
+//     res.status(200).send()
+// })
+
+// server.put('/:value', (req, res) => {
+//     const value = req.params.value
+//     const id = req.query.id
+//     console.log(`QUERY IS ${id} AND PARAMETER IS ${value}`)
     
-    temperatures.map(temperature => {
-        if(temperature.id == id){
-            console.log(`FOUND ID ${id} CHANGING VALUE OF OBJECT`)
-            temperature.temperature = value
-        }
-    });
+//     temperatures.map(temperature => {
+//         if(temperature.id == id){
+//             console.log(`FOUND ID ${id} CHANGING VALUE OF OBJECT`)
+//             temperature.temperature = value
+//         }
+//     });
 
-    res.status(200).send()
-})
+//     res.status(200).send()
+// })
 
 async function main() {
     await mongoose.connect('mongodb+srv://luanmarcal:LuanMiguel@lm-labs.mjhnf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
-    server.listen(port, ()=>{
-        console.log(`o servidor está rodando em ${port}`)
+    server.listen(port, () => {
+        console.log(`server running on port ${port}`)
     })
 }
 
---
+++ src/routes/temperature.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const router = express.Router()
+const Temperature = require('../models/Temperature')
+
+const expressValidator = require('express-validator')
+
+const validate = [
+    expressValidator.check('temperature').isLength({min: 1}).withMessage('Field temperature can not be null'),
+    expressValidator.check('temperature').isNumeric().withMessage('Field temperature should be a number')
+]
+
+router.get('/', (req, res) => {
+    Temperature.find().then(temperatures => {
+        res.status(200).send(temperatures);
+    }).catch(error => {
+        res.status(500).send(error)
+    })
+})
+
+router.get('/:id', (req, res) => {
+
+    Temperature.findById(req.params.id)
+    .then(temperature => {
+        res.status(200).send(temperature)
+    }).catch(error => {
+        res.status(404);
+    })
+
+})
+
+router.post('/', [validate], (req, res) => {
+
+    const erros = expressValidator.validationResult(req);
+    if(!erros.isEmpty()){
+        return res.status(422).send({erros: erros.array()})
+    }
+
+    const temperature = new Temperature({
+        temperature: req.body.temperature
+    })
+
+    temperature.save()
+    .then(result => {
+        res.status(201).send(result)
+    })
+})
+
+router.delete('/', (req, res) => {
+    
+    Temperature.deleteMany().then(result => {
+        res.status(200).send()
+    });
+})
+
+router.delete('/query', (req, res) => {
+    
+    Temperature.findByIdAndDelete(req.query.id)
+    .then(result => {
+        res.status(200).send(result)
+    }).catch( error => {
+        res.status(404).send()
+    })
+})
+
+router.put('/:value', (req, res) => {
+    const pathValue = req.params.value
+
+    Temperature.findById(req.query.id).then(temperature => {
+        temperature.temperature = pathValue
+        temperature.save().then(result => {
+            res.status(200).send(result)
+        })
+    }).catch( error => {
+        res.status(404).send()
+    })
+
+})
+
+module.exports = router;
